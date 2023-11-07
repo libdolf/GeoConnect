aff060b6b8becb58300a8f41fe510ff3adb46345
Lindolfo Moizinho <lindolfomoizinho1@gmail.com>
2023-11-07 09:43:17 -0300

    Initial commit

---
+++ src/main/java/br/com/libdolf/geoconnect/GeoConnectApplication.java
@@ -0,0 +1,13 @@
+package br.com.libdolf.geoconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoConnectApplication.class, args);
+    }
+
+}
